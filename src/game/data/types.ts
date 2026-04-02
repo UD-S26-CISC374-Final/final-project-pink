9185b407c6bc20c3262348c5002c08d0eb38a520
Tommy-Parisi <tparisi@udel.edu>
2026-04-02 11:42:03 -0400

    plan: defined data types

---
+++ src/game/data/types.ts
@@ -0,0 +1,32 @@
+export type Verdict = "guilty" | "not guilty";
+
+
+export interface UnitTest {
+    id: string;
+    label: string; // e.g. "AbsoluteValue(5) -> 5"
+    input: string;
+    expectedOutput: string;
+    actualOutput: string;
+    passes: boolean;
+    showResult: boolean; // If we want to show the result of the test or have the player figure it out
+}   
+
+export interface TestFeedback {
+    testId: string;
+    quality: 'essential' | 'redundant' | 'misleading';
+    feedback: string; // What judge compiler will say about this test
+}
+
+export interface Case {
+    id: string;
+    title: string;
+    isTutorial: boolean;
+    functionCode: string; // Displayed in the case file function box, this is the function on trial
+    description: string; // What the function should do
+    evidencePool: UnitTest[]; // All the tests that the player can choose from
+    evidenceSlots: number; // evidence card slots available
+    correctVerdict: Verdict; 
+    testFeedback: TestFeedback[]; 
+    missedEvidenceExplanation: string; 
+    closingStatement: string;
+}
