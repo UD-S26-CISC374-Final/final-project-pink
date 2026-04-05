9acb64546394ec487ca135232c11b3b8d3e567cf
Hairum Qureshi <hqureshi@udel.edu>
2026-04-05 12:45:51 -0400

    changed game title to 'Case By Case'

--- src/game/main.ts
+++ src/game/main.ts
@@ -8,7 +8,7 @@ import { Preloader } from "./scenes/preloader";
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Phaser.Types.Core.GameConfig = {
-    title: "My Untitled CISC374 Game",
+    title: "Case By Case",
     version: "0.0.1",
     type: AUTO,
     parent: "game-container",
