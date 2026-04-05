e286681ae940d67d6aeca7c447424e0f2347fcef
Hairum Qureshi <hqureshi@udel.edu>
2026-04-05 12:06:37 -0400

    created Difficulty type and added it to Case interfacE

--- src/game/data/types.ts
+++ src/game/data/types.ts
@@ -1,4 +1,5 @@
 export type Verdict = "guilty" | "not guilty";
+export type Difficulty = "easy" | "medium" | "hard";
 
 export interface UnitTest {
     id: string;
@@ -8,23 +9,24 @@ export interface UnitTest {
     actualOutput: string;
     passes: boolean;
     showResult: boolean; // If we want to show the result of the test or have the player figure it out
-}   
+}
 
 export interface TestFeedback {
     testId: string;
-    quality: 'essential' | 'redundant' | 'misleading';
+    quality: "essential" | "redundant" | "misleading";
     feedback: string; // What judge compiler will say about this test
 }
 
 export interface Case {
     id: string;
+    difficulty: Difficulty;
     title: string;
     functionCode: string; // Displayed in the case file function box, this is the function on trial
     description: string; // What the function should do
     evidencePool: UnitTest[]; // All the tests that the player can choose from
     evidenceSlots: number; // evidence card slots available
-    correctVerdict: Verdict; 
-    testFeedback: TestFeedback[]; 
-    missedEvidenceExplanation: string; 
+    correctVerdict: Verdict;
+    testFeedback: TestFeedback[];
+    missedEvidenceExplanation: string;
     closingStatement: string;
 }
