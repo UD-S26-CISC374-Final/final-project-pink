5c9434aeb190d663abdc7366dcd27fff6e0e1001
Hairum Qureshi <hqureshi@udel.edu>
2026-04-13 08:41:34 -0400

    removed 'moveSprite' method

--- src/game/scenes/main-menu.ts
+++ src/game/scenes/main-menu.ts
@@ -7,7 +7,6 @@ export class MainMenu extends Scene implements ChangeableScene {
     background: GameObjects.Image;
     logo: GameObjects.Image;
     title: GameObjects.Text;
-    logoTween: Phaser.Tweens.Tween | null;
     tutorialChosen: boolean;
     gamemode: "Level1" | "Tutorial";
 
@@ -65,30 +64,6 @@ export class MainMenu extends Scene implements ChangeableScene {
             this.scene.start("Tutorial");
         } else {
             this.scene.start("Level1");
-        }
-    }
-
-    moveSprite(callback: ({ x, y }: { x: number; y: number }) => void) {
-        if (this.logoTween) {
-            if (this.logoTween.isPlaying()) {
-                this.logoTween.pause();
-            } else {
-                this.logoTween.play();
-            }
-        } else {
-            this.logoTween = this.tweens.add({
-                targets: this.logo,
-                x: { value: 750, duration: 3000, ease: "Back.easeInOut" },
-                y: { value: 80, duration: 1500, ease: "Sine.easeOut" },
-                yoyo: true,
-                repeat: -1,
-                onUpdate: () => {
-                    callback({
-                        x: Math.floor(this.logo.x),
-                        y: Math.floor(this.logo.y),
-                    });
-                },
-            });
         }
     }
 }
