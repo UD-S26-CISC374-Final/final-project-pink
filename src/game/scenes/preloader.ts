723f2e1fbcb803ed6cf5c70871af29c516436af5
Hairum Qureshi <hqureshi@udel.edu>
2026-04-13 08:25:21 -0400

    removed custom image loader

--- src/game/scenes/preloader.ts
+++ src/game/scenes/preloader.ts
@@ -24,7 +24,6 @@ export class Preloader extends Scene {
     preload() {
         //  Load the assets for the game - Replace with your own assets
         this.load.setPath("assets");
-        this.load.image("title-screen", "case-by-case-title-screen.jpg");
     }
 
     create() {
