6c926ca9835a456ea7c9389298f03b1863f86df2
Hairum Qureshi <hqureshi@udel.edu>
2026-04-17 19:55:01 -0400

    refined logic for typewriterEffect util function and modified params

--- src/game/utils/typeWriterAnimation.ts
+++ src/game/utils/typeWriterAnimation.ts
@@ -1,26 +1,28 @@
-export async function typewriterEffect(
-    judge: Phaser.GameObjects.Sprite,
-    typingInProgress: boolean,
+export function typewriterEffect(
+    judge: Phaser.GameObjects.Sprite | null,
     target: Phaser.GameObjects.Text,
+    message: string,
     speedInMS: number = 50,
 ) {
     // credit for code: https://joel.net/creating-a-typewriter-effect-in-phaserjs-v3
-    const message = target.text;
+    // code altered from the original, but overall logic and structure is the same
+
     const invisibleMessage = message.replace(/[^ ]/g, " ");
-    target.text = "";
+    target.setText("");
+
     let visibleText = "";
 
     return new Promise<void>((resolve) => {
-        typingInProgress = true;
         const timer = target.scene.time.addEvent({
             delay: speedInMS,
             loop: true,
             callback: () => {
-                if (target.text === message) {
+                if (visibleText.length >= message.length) {
                     timer.destroy();
-                    judge.anims.pause();
-                    judge.setFrame(0);
-                    typingInProgress = false;
+                    if (judge) {
+                        judge.anims.pause();
+                        judge.setFrame(0);
+                    }
                     resolve();
                     return;
                 }
@@ -29,6 +31,7 @@ export async function typewriterEffect(
                 const invisibleText = invisibleMessage.substring(
                     visibleText.length,
                 );
+
                 target.setText(visibleText + invisibleText);
             },
         });
