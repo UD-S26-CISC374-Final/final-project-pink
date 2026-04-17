05887c7b3019f4542351cc4272db726f24de1867
Hairum Qureshi <hqureshi@udel.edu>
2026-04-17 09:02:34 -0400

    created typewriterEffect method, code to display a 'next' button and logic to render the next set of text and animated sprite after hitting 'next'

--- src/game/scenes/tutorial.ts
+++ src/game/scenes/tutorial.ts
@@ -3,18 +3,76 @@ import tutorialCases from "../data/tutorial-cases.json";
 
 export class Tutorial extends Scene {
     judge: Phaser.GameObjects.Sprite;
+    index: number = 0;
+    typingInProgress: boolean = false;
 
     constructor() {
         super("Tutorial");
     }
 
-    create() {
-        this.cameras.main.setBackgroundColor("#2d2d2d");
+    private playGiveCaseFileAnimation() {
+        this.anims.create({
+            key: "give-case-file",
+            frames: this.anims.generateFrameNumbers(
+                "judge-compiler-case-sprite",
+                {
+                    frames: [0, 1, 2],
+                },
+            ),
+            frameRate: 3,
+            repeat: -1,
+        });
 
-        const fullText =
-            "cout << \"Welcome to the tutorial! I'm The Honorable Judge Compiler, and I'll be your guide as you learn the basics of being a lawyer at the Syntax Criminal Court! To start, let's familiarize ourselves with the interface you'll be using to dissect each case.\" << endl;";
-        let currentText = "";
-        let index = 0;
+        this.judge = this.add
+            .sprite(
+                0,
+                this.cameras.main.height,
+                "judge-compiler-case-sprite",
+                0,
+            )
+            .setOrigin(0, 1)
+            .setScale(3);
+
+        this.judge.play("give-case-file");
+    }
+
+    private typewriterEffect(
+        target: Phaser.GameObjects.Text,
+        speedInMS: number = 50,
+    ) {
+        // credit for code: https://joel.net/creating-a-typewriter-effect-in-phaserjs-v3
+        const message = target.text;
+        const invisibleMessage = message.replace(/[^ ]/g, " ");
+        target.text = "";
+        let visibleText = "";
+
+        return new Promise<void>((resolve) => {
+            this.typingInProgress = true;
+            const timer = target.scene.time.addEvent({
+                delay: speedInMS,
+                loop: true,
+                callback: () => {
+                    if (target.text === message) {
+                        timer.destroy();
+                        this.judge.anims.pause();
+                        this.judge.setFrame(0);
+                        this.typingInProgress = false;
+                        resolve();
+                        return;
+                    }
+
+                    visibleText += message[visibleText.length];
+                    const invisibleText = invisibleMessage.substring(
+                        visibleText.length,
+                    );
+                    target.setText(visibleText + invisibleText);
+                },
+            });
+        });
+    }
+
+    async create() {
+        this.cameras.main.setBackgroundColor("#2d2d2d");
 
         this.add.rectangle(512, 130, 1024, 200, 0x000000, 0.8).setOrigin(0.5);
 
@@ -27,20 +85,10 @@ export class Tutorial extends Scene {
             })
             .setOrigin(0.5);
 
-        this.time.addEvent({
-            delay: 50,
-            repeat: fullText.length - 1,
-            callback: () => {
-                currentText += fullText[index];
-                textObject.setText(currentText);
-                index++;
-
-                if (index === fullText.length) {
-                    this.judge.anims.pause();
-                    this.judge.setFrame(0);
-                }
-            },
-        });
+        const tutorialText = [
+            "cout << \"Welcome to the tutorial! I'm The Honorable Judge Compiler, and I'll be your guide as you learn the basics of being a lawyer at the Syntax Criminal Court! To start, let's familiarize ourselves with the interface you'll be using to dissect each case.\" << endl;",
+            'cout << "This is a case file. It contains all the information about a case. Each case file contains the program, the purpose it claims to serve, and a series of test cases that either prove or disprove its innocence. It\'s up to you to determine that based on the presented evidence." << endl;',
+        ];
 
         this.anims.create({
             key: "talk",
@@ -57,6 +105,42 @@ export class Tutorial extends Scene {
             .setScale(3);
 
         this.judge.play("talk");
+
+        await this.typewriterEffect(textObject.setText(tutorialText[0]));
+
+        const buttonContainer = this.add.container(512, 300).setAlpha(0);
+
+        const buttonBackground = this.add
+            .rectangle(0, 0, 200, 50, 0x000000, 0.8)
+            .setOrigin(0.5);
+
+        const buttonText = this.add
+            .text(0, 0, "Next", {
+                fontFamily: "Google Sans Code",
+                fontSize: 20,
+                color: "#01ff34",
+            })
+            .setOrigin(0.5);
+
+        buttonContainer.add([buttonBackground, buttonText]);
+
+        buttonContainer.setSize(buttonText.width, buttonText.height);
+        buttonContainer.setInteractive();
+
+        this.tweens.add({
+            targets: buttonContainer,
+            alpha: 1,
+            duration: 1000,
+            ease: "Power2",
+        });
+
+        buttonContainer.on("pointerdown", async () => {
+            if (this.typingInProgress) return;
+            this.judge.setTexture("judge-compiler-case-sprite");
+            this.playGiveCaseFileAnimation();
+            buttonContainer.destroy();
+            await this.typewriterEffect(textObject.setText(tutorialText[1]));
+        });
     }
 
     update() {}
