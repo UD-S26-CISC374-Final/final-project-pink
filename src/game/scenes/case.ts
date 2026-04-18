974e2bbc925df18ed62aed2ff1a589e527ef1d9b
Hairum Qureshi <hqureshi@udel.edu>
2026-04-18 01:24:21 -0400

    added functioning toggling between going back to the game start menu, switching from one tab to another

--- src/game/scenes/case.ts
+++ src/game/scenes/case.ts
@@ -12,29 +12,35 @@ export class Case extends Scene {
     nextTutorialText: string;
     typingInProgress: boolean = false;
     textObject: Phaser.GameObjects.Text;
-    showTestCases = false;
-    showProgramPurpose = false;
+    currentTab: "code" | "explanation" | "test-cases";
     currTutorialCaseIndex = 0;
     currTutorialCaseDesc =
         tutorialCases[this.currTutorialCaseIndex].description;
+    caseFileCodeSnippet: Phaser.GameObjects.Image;
     programDescTextReference: Phaser.GameObjects.Text;
+    backButton: Phaser.GameObjects.Container;
 
-    // TODO - clean up the sloppy code, make helper functions, there's a lot of duplicate code
-    // TODO - have it so that when you click on the tab, instead of having to wait for the dialogue to finish, the user can click back immediately and it will stop the current dialogue and go back to the previous one
+    // ! For some reason, switching to the MainMenu scene isn't working properly
 
-    init(data: {
-        isTutorial: boolean;
-        nextTutorialText: string;
-        judge: Phaser.GameObjects.Sprite;
-    }) {
-        this.cameras.main.setBackgroundColor("#2d2d2d");
-        this.add.rectangle(512, 80, 1024, 120, 0x000000, 0.8).setOrigin(0.5);
+    private async goBack() {
+        this.textObject.setText("");
+        this.programDescTextReference.destroy();
+        this.caseFileCodeSnippet = this.add
+            .image(512, 450, "tutorial-code-1")
+            .setDisplaySize(920, 600)
+            .setScale(0.26)
+            .setDepth(10);
 
-        this.isTutorial = data.isTutorial;
-        this.nextTutorialText = data.nextTutorialText;
+        this.add
+            .sprite(512, 450, "case-file-open-program", 0)
+            .setDisplaySize(920, 600);
+
+        if (this.currentTab !== "code") this.currentTab = "code";
+
+        await this.addAnimatedTypingText(this.nextTutorialText);
     }
 
-    async create() {
+    private drawTabs() {
         const greenTab = this.add
             .rectangle(870, 190, 148, 80, 0x00ff00, 0.8)
             .setOrigin(0.5)
@@ -55,104 +61,107 @@ export class Case extends Scene {
             .setAlpha(0.09)
             .setInteractive();
 
-        const caseFileCodeSnippet = this.add
-            .image(512, 450, "tutorial-code-1")
-            .setDisplaySize(920, 600)
-            .setScale(0.4)
-            .setDepth(10);
-
         pinkTab.on("pointerdown", async () => {
             if (this.typingInProgress) return;
-            caseFileCodeSnippet.destroy();
+            this.caseFileCodeSnippet.destroy();
+            this.currentTab = "explanation";
+            this.textObject.setText("");
 
             const fourthIntro =
                 'cout << "Here is the program\'s statement of purpose, which gives a brief overview of what the program is supposed to do. This can help guide your analysis of the program and its test cases." << endl;';
+            await this.addAnimatedTypingText(fourthIntro);
 
-            // this adds the current tutorial case's program description
             this.programDescTextReference = this.add
                 .text(512, 350, this.currTutorialCaseDesc, {
                     fontFamily: "Google Sans Code",
                     fontSize: 23,
                     color: "#ffffff",
-                    wordWrap: { width: 600, useAdvancedWrap: true },
+                    wordWrap: {
+                        width: 600,
+                        useAdvancedWrap: true,
+                    },
                 })
                 .setOrigin(0.5);
-
-            this.typingInProgress = true;
-            await typewriterEffect(
-                null,
-                this.textObject.setText(fourthIntro),
-                fourthIntro,
-            );
-            this.typingInProgress = false;
-
-            const backButton = createTextButton
-                .call(
-                    this,
-                    150,
-                    190,
-                    {
-                        x: 0,
-                        y: 0,
-                        width: 100,
-                        height: 40,
-                        color: 0x000000,
-                        alpha: 0.8,
-                    },
-                    {
-                        text: "Go Back",
-                        fontFamily: "Google Sans Code",
-                        fontSize: 18,
-                        color: "#ffffff",
-                    },
-                    true,
-                )
-                .setDepth(100);
-
-            backButton.on("pointerdown", async () => {
-                if (this.typingInProgress) return;
-                this.textObject.setText("");
-                this.programDescTextReference.destroy();
-                this.add
-                    .image(512, 450, "tutorial-code-1")
-                    .setDisplaySize(920, 600)
-                    .setScale(0.4)
-                    .setDepth(10);
-                backButton.destroy();
-
-                this.typingInProgress = true;
-                await typewriterEffect(
-                    null,
-                    this.textObject.setText(this.nextTutorialText),
-                    this.nextTutorialText,
-                );
-                this.typingInProgress = false;
-            });
         });
 
+        this.backButton = createTextButton
+            .call(
+                this,
+                150,
+                190,
+                {
+                    x: 0,
+                    y: 0,
+                    width: 100,
+                    height: 40,
+                    color: 0x000000,
+                    alpha: 0.8,
+                },
+                {
+                    text: "Go Back",
+                    fontFamily: "Google Sans Code",
+                    fontSize: 18,
+                    color: "#ffffff",
+                },
+                true,
+            )
+            .setDepth(100);
+        this.backButton.on("pointerdown", async () => {
+            if (this.currentTab === "code") {
+                const confirmation = confirm(
+                    "Are you sure you want to go back? Your progress in the tutorial will be lost.",
+                );
+                if (!confirmation) return;
+
+                this.scene.stop("Tutorial");
+                this.scene.start("MainMenu");
+                return;
+            }
+
+            await this.goBack();
+        });
+    }
+
+    async addAnimatedTypingText(text: string) {
+        this.typingInProgress = true;
+
+        this.textObject = this.add.text(100, 30, "", {
+            fontSize: "21px",
+            color: "#01ff34",
+            wordWrap: { width: 800 },
+        });
+
+        await typewriterEffect(null, this.textObject.setText(text), text, 1);
+        this.typingInProgress = false;
+    }
+
+    init(data: {
+        isTutorial: boolean;
+        nextTutorialText: string;
+        judge: Phaser.GameObjects.Sprite;
+    }) {
+        this.cameras.main.setBackgroundColor("#2d2d2d");
+        this.add.rectangle(512, 80, 1024, 120, 0x000000, 0.8).setOrigin(0.5);
+        this.currentTab = "code";
+        this.isTutorial = data.isTutorial;
+        this.nextTutorialText = data.nextTutorialText;
+    }
+
+    async create() {
+        // 1. first, we are going to introduce the user with the next tutorial's text
+        await this.addAnimatedTypingText(this.nextTutorialText);
+
+        // 2. Next, we are going to display the open case file sprite showing the program's code and adding the clickable tabs as well
         this.add
             .sprite(512, 450, "case-file-open-program", 0)
             .setDisplaySize(920, 600);
+        this.drawTabs();
 
-        this.textObject = this.add
-            .text(512, 75, "", {
-                fontFamily: "Google Sans Code",
-                fontSize: 20,
-                color: "#01ff34",
-                wordWrap: { width: 950, useAdvancedWrap: true },
-            })
-            .setOrigin(0.5);
-
-        this.typingInProgress = true;
-
-        await typewriterEffect(
-            null,
-            this.textObject.setText(this.nextTutorialText),
-            this.nextTutorialText,
-            1,
-        ); // TODO - remove 1
-
-        this.typingInProgress = false;
+        // 3. Next, we will display the code snippet for the case file
+        this.caseFileCodeSnippet = this.add
+            .image(512, 450, "tutorial-code-1")
+            .setDisplaySize(920, 600)
+            .setScale(0.26)
+            .setDepth(10);
     }
-    update() {}
 }
