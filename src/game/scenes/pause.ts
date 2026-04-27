d237efe40485ed33d587c0638f85eaf272b5f0d1
Hairum Qureshi <hqureshi@udel.edu>
2026-04-27 01:54:40 -0400

    removed missing cout and endl text from dialogue text

--- src/game/scenes/pause.ts
+++ src/game/scenes/pause.ts
@@ -190,7 +190,7 @@ export class Pause extends Scene {
             this.showMainMenuButton();
 
             await this.addAnimatedTypingText(
-                'cout << "Progress saved! Next time you come back, you\'ll continue from where you left off." << endl;',
+                "Progress saved! Next time you come back, you\'ll continue from where you left off.",
                 20,
             );
         });
