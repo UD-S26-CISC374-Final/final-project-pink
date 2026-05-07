50e8b19dfa7af9484ee2f7912b09f8f0b3d03b6b
Hairum Qureshi <hqureshi@udel.edu>
2026-05-07 03:36:56 -0400

    removed algorithm for registering all code image assets since it's no longer needed now

--- src/game/scenes/preloader.ts
+++ src/game/scenes/preloader.ts
@@ -65,22 +65,6 @@ export class Preloader extends Scene {
             frameHeight: 32,
         });
 
-        let i = 0;
-        while (i < tutorialCases.length) {
-            this.load.image(
-                `tutorial-code-${i}`,
-                `tutorial-cases-code/tutorial-${i}.png`,
-            );
-
-            for (let j = 1; j <= tutorialCases[i].testFeedback.length; j++) {
-                this.load.image(
-                    `tutorial-${i}-t${j}`,
-                    `tutorial-cases-code/test-cases/tutorial-${i}-t${j}.png`,
-                );
-            }
-            i++;
-        }
-
         this.load.audio("theme-music", ["audio/game-music.mp3"]);
 
         this.load.font("Google Sans Code", "fonts/Google-Sans-Code.ttf");
