cf94412dc9b8d2fd17a1223238bf4d363e0fee1d
Hairum Qureshi <hqureshi@udel.edu>
2026-05-12 00:10:58 -0400

    registered game background music

--- src/game/scenes/preloader.ts
+++ src/game/scenes/preloader.ts
@@ -66,6 +66,9 @@ export class Preloader extends Scene {
 
         this.load.audio("theme-music", ["audio/game-menu-music.mp3"]);
         this.load.audio("button-click", ["audio/button-click.wav"]);
+        this.load.audio("background-music", [
+            "audio/game-background-music.wav",
+        ]);
 
         this.load.font("Google Sans Code", "fonts/Google-Sans-Code.ttf");
         this.load.image("gavel", "gavel.png");
