6625e08a174d2b6614e2254d6e33ceb3004b767a
Hairum Qureshi <hqureshi@udel.edu>
2026-05-13 00:38:21 -0400

    modified 'getCaseData' so it no longer takes in the difficulty and also added conditional to utilize an algorithm for generating a randomized 15 case array for the main game

--- src/game/utils/getCaseData.ts
+++ src/game/utils/getCaseData.ts
@@ -9,10 +9,34 @@ const mediumCases = mediumCasesJSON as Case[];
 const hardCases = hardCasesJSON as Case[];
 const tutorialCases = tutorialCasesJSON as Case[];
 
+function getRandomFiveCases(
+    difficultyLevel: "easy" | "medium" | "hard",
+): Case[] {
+    const randomized: Case[] = [];
+    let current = 0;
+    const numRounds = 5;
+    while (current !== numRounds) {
+        const randomIndex = Math.floor(Math.random() * numRounds);
+        if (!randomized[randomIndex]) {
+            if (difficultyLevel === "easy") {
+                randomized[randomIndex] = easyCases[current];
+                current++;
+            } else if (difficultyLevel === "medium") {
+                randomized[randomIndex] = mediumCases[current];
+                current++;
+            } else {
+                randomized[randomIndex] = hardCases[current];
+                current++;
+            }
+        }
+    }
+
+    return randomized;
+}
+
 export default function getCaseData(
     isTutorial: boolean,
-    levelDifficulty: "easy" | "medium" | "hard",
-    currTutorialCaseIndex: number,
+    currentCaseIndex: number,
 ): {
     feedback: UnitTest[];
     case: string;
@@ -20,31 +44,57 @@ export default function getCaseData(
 } {
     if (isTutorial) {
         return {
-            feedback: tutorialCases[currTutorialCaseIndex]?.evidencePool || [],
-            case: tutorialCases[currTutorialCaseIndex]?.functionCode,
-            currCaseData: tutorialCases[currTutorialCaseIndex].testFeedback,
+            feedback: tutorialCases[currentCaseIndex]?.evidencePool || [],
+            case: tutorialCases[currentCaseIndex]?.functionCode,
+            currCaseData: tutorialCases[currentCaseIndex].testFeedback,
         };
-    }
+    } else {
+        const endAmountOfCases = 15;
+        let current = 0;
+        const difficultyLevels = ["easy", "medium", "hard"];
+        const difficultyCount = 3; // the number of difficulty levels
+        let currentDifficulty = 1;
+        const randomizedCasesArr: Case[] = [];
+        const savedRandomizedCases = localStorage.getItem("randomizedCases");
+        if (!savedRandomizedCases) {
+            while (
+                current !== endAmountOfCases &&
+                currentDifficulty !== difficultyCount
+            ) {
+                const randomFiveCases: Case[] = getRandomFiveCases(
+                    difficultyLevels[currentDifficulty - 1] as
+                        | "easy"
+                        | "medium"
+                        | "hard",
+                );
 
-    if (levelDifficulty === "easy") {
-        return {
-            feedback: easyCases[currTutorialCaseIndex]?.evidencePool || [],
-            case: easyCases[currTutorialCaseIndex]?.functionCode,
-            currCaseData: easyCases[currTutorialCaseIndex].testFeedback,
-        };
-    }
+                randomizedCasesArr.push(...randomFiveCases);
+                currentDifficulty++;
+                current += 5;
+            }
 
-    if (levelDifficulty === "medium") {
-        return {
-            feedback: mediumCases[currTutorialCaseIndex]?.evidencePool || [],
-            case: mediumCases[currTutorialCaseIndex]?.functionCode,
-            currCaseData: mediumCases[currTutorialCaseIndex].testFeedback,
-        };
-    }
+            localStorage.setItem(
+                "randomizedCases",
+                JSON.stringify(randomizedCasesArr),
+            );
 
-    return {
-        feedback: hardCases[currTutorialCaseIndex]?.evidencePool || [],
-        case: hardCases[currTutorialCaseIndex]?.functionCode,
-        currCaseData: hardCases[currTutorialCaseIndex].testFeedback,
-    };
+            return {
+                feedback:
+                    randomizedCasesArr[currentCaseIndex]?.evidencePool || [],
+                case: randomizedCasesArr[currentCaseIndex]?.functionCode,
+                currCaseData: randomizedCasesArr[currentCaseIndex].testFeedback,
+            };
+        } else {
+            const parsedRandomizedCases: Case[] = JSON.parse(
+                savedRandomizedCases,
+            ) as Case[];
+            return {
+                feedback:
+                    parsedRandomizedCases[currentCaseIndex]?.evidencePool || [],
+                case: parsedRandomizedCases[currentCaseIndex]?.functionCode,
+                currCaseData:
+                    parsedRandomizedCases[currentCaseIndex].testFeedback,
+            };
+        }
+    }
 }
